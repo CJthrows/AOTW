87b15907987b5f26bc41729845b4c9adad077976
CJthrows <143540700+CJthrows@users.noreply.github.com>
2024-03-28 09:50:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.onload = function() {
+    const images = document.querySelectorAll('.collage img');
+    let index = 0;
+
+    setInterval(() => {
+        images[index].classList.remove('active');
+        index = (index + 1) % images.length;
+        images[index].classList.add('active');
+    }, 3000);
+};
 
