1ce5fa67abc49aabef90ad273d6134ce6ff23a12
CJthrows <143540700+CJthrows@users.noreply.github.com>
2024-03-28 15:49:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 window.onload = function() {
     const images = document.querySelectorAll('.collage img');
     let index = 0;
+    let timer;
 
-    setInterval(() => {
-        images[index].classList.remove('active');
+    function showImage(indexToShow) {
+        for (let i = 0; i < images.length; i++) {
+            images[i].classList.remove('active');
+        }
+        images[indexToShow].classList.add('active');
+    }
+
+    function prevImage() {
+        index = (index - 1 + images.length) % images.length;
+        showImage(index);
+        resetTimer();
+    }
+
+    function nextImage() {
         index = (index + 1) % images.length;
-        images[index].classList.add('active');
-    }, 3000);
+        showImage(index);
+        resetTimer();
+    }
+    function resetTimer() {
+        clearInterval(timer);
+        timer = setInterval(nextImage, 3000); 
+    }
+    document.querySelector('.collage .controls button:nth-child(1)').addEventListener('click', prevImage);
+    document.querySelector('.collage .controls button:nth-child(2)').addEventListener('click', nextImage);
+    resetTimer();
 };
-
